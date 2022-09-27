3a919b037a31b61ff36005498000715f27f10e2b
Tayam0nline <89915786+Tayam0nline@users.noreply.github.com>
2022-09-27 11:22:19 +0200

    Update Program.cs

--- The-Garden-Group-Project/Program.cs
+++ The-Garden-Group-Project/Program.cs
@@ -17,7 +17,7 @@ namespace The_Garden_Group_Project
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
