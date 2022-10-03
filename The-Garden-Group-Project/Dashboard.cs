ea74863291d0184d191f265a353c324e8766b52f
DaanTol <686379@student.inholland.nl>
2022-10-03 19:40:38 +0200

    Added a form

---
+++ The-Garden-Group-Project/Dashboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace The_Garden_Group_Project
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
