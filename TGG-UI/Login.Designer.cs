8efdd979fc1b7e56519f6e82711348b75652bf1c
Tayam0nline <89915786+Tayam0nline@users.noreply.github.com>
2022-10-06 12:30:15 +0200

    Revert "Merge remote-tracking branch 'origin/Dashboard' into development"
    This reverts commit e09d7d61d33e431f0bad8bfba63b935309d0322c, reversing
    changes made to 97a1ac52e7045df5b4ae68d0f28d86d31c7cc684.

--- TGG-UI/Login.Designer.cs
+++ TGG-UI/Login.Designer.cs
@@ -33,7 +33,7 @@ namespace TGG_UI
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Login";
