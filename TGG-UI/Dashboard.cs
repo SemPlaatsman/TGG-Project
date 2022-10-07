c18362ae63bdd6960b5e222376d98d8d393e5663
Tayam0nline <89915786+Tayam0nline@users.noreply.github.com>
2022-10-07 18:40:27 +0200

    Dashboard begin

---
+++ TGG-UI/Dashboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TGG_Model;
+using TGG_Logic;
+
+namespace TGG_UI
+{
+    public partial class Dashboard : Form
+    {
+        private TicketService ticketService;
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            ticketService = new TicketService();
+            aa();
+        }
+        public void aa()
+        {
+            List<Ticket> tickets = ticketService.GetAllTickets();
+
+
+        }
+
+    }
+}
