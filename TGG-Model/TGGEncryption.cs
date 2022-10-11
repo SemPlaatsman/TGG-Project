1d031c9f8d08ee4593424a3b8934f63dcc0de14f
SemPlaatsman <semplaatsman@gmail.com>
2022-10-11 11:10:03 +0200

    Worked on archive

---
+++ TGG-Model/TGGEncryption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Encryption;
+
+namespace TGG_Model
+{
+    public class TGGEncryption
+    {
+    }
+}
